dac7ff2bd5ce4ae0881caf0399a8703bf24d0b56
Maryna <maryna@thinkingbox.com>
2019-10-28 17:19:54 -0400

    js func

--- index.js
+++ index.js
@@ -1,11 +1,40 @@
-let display= "0";
-buffer
-
-var display = document.querySelector('.screen');
-
-
-const number = document.querySelector('.buttons').addEventListener('click', function(event) {
-      display.innerText = event.target.innerText;
-    });
+let runningTotal = 0;
+let buffer = "0"; //keep track what user presses
+let previousOperator //keep track what user previously pressed;
+const screen = document.querySelector('.screen');
 
 
+//1. Bind up event listiner
+
+document.querySelector('.buttons').addEventListener('click', function (event) {
+    buttonClick(event.target.innerText);
+})
+
+//2. Write function button click taht check is prssed button is number or symbol
+
+function buttonClick(value) {
+    if (isNaN(parseInt(value))) {
+        handleSymbol(value);
+    } else {
+        handleNumber(value);
+    }
+
+}
+
+function handleNumber(value) {
+    if (buffer === '0') {
+        buffer = value;
+    } else {
+        buffer += value;
+    }
+    rerender()
+}
+
+function handleSymbol(value) {
+
+}
+
+
+function rerender() {
+    screen.innerText = buffer;
+}
